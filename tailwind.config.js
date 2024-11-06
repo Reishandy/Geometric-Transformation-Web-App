851b8dc0373d9e21ecbfda0600ccf22d8ca3d1fa
Reishandy <isthisruxury@gmail.com>
2024-11-06 18:16:14 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      display: ['Pally', 'Comic Sans MS', 'sans-serif'],
+      body: ['Pally', 'Comic Sans MS', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
